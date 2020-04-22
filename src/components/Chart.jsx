7159895b0131aac8c8f7d064fc9088a834919064
Jacqueline Remmel <jpremmel@gmail.com>
2020-04-22 13:26:56 -0700

    Swith to doughnut chart and update colors

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Bar } from 'react-chartjs-2';
+import { Doughnut } from 'react-chartjs-2';
 
 const Chart = () => {
+
   const chartData = {
     labels: ['Honeymoon', 'Student Loan Debt', 'Our First Home', 'Therapy for Our Future Kids'],
     datasets: [
       {
-        label: 'Amount',
-        backgroundColor: 'rgba(75,192,192,1)',
-        borderColor: 'rgba(0,0,0,1)',
+        label: 'Percentage of total gift funds',
+        backgroundColor: ["#ffd480", "#e6d7f4","#a6e8e6","#803370"],
+        borderColor: ["#ffd480", "#ded7f4","#a6e8e6","#803370"],
         borderWidth: 2,
         data: [20, 40, 70, 50]
       }
@@ -17,16 +18,13 @@ const Chart = () => {
 
   return (
     <div className='container'>
-      <Bar
+      <Doughnut
         data={chartData}
         options={{
           title: {
             display: true,
             text: 'Caitlin & Dimitar\'s Wedding Registry Funds',
             fontSize: 20
-          },
-          legend:{
-            display: false
           }
         }}
       />
